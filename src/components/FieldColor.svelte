82407a09a5eedf323f6f55aadc5aee4a841a3a40
Ewen Le Bihan <hey@ewen.works>
2021-12-23 19:21:49 +0100

    💄 <FieldColor> Finish styling of unset swatches

--- src/components/FieldColor.svelte
+++ src/components/FieldColor.svelte
@@ -15,22 +15,39 @@ function reset(e) {
 	<div
 		class="swatch"
 		class:unset={!value}
-		style={value ? `background-color: #${value};` : ""}
+		style={value
+			? `background-color: #${value};border-color: #${value};`
+			: ""}
 		on:dblclick={reset}
 	/>
 	<input type="text" bind:value placeholder="unset" />
 </MetadataField>
 
 <style>
-	.swatch {
-		width:  1em;
-		height: 1em;
-		border-radius: 100%;
-		display: inline-block;
-		margin-left: 1em;
-		margin-right: .5em;
-	}
-	.swatch.unset {
-		background-color: var(--gray);
-	}
+.swatch {
+	width: 1em;
+	height: 1em;
+	border-radius: 100%;
+	display: inline-block;
+	margin-left: 1em;
+	margin-right: 0.5em;
+	border: 0.2em solid;
+}
+.swatch.unset {
+	border-color: var(--gray);
+	position: relative;
+}
+.swatch.unset::after {
+	content: "";
+	position: absolute;
+	top: 50%;
+	transform: translateY(-50%) rotate(-45deg);
+	width: 100%;
+	height: 0.2em;
+	background: var(--gray);
+}
+input {
+	border: none;
+	font-size: 1em;
+}
 </style>
