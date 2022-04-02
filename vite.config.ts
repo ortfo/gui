239d1251316d5e44145c615269b0da5616364fad
Ewen Le Bihan <hey@ewen.works>
2022-04-02 22:13:14 +0200

    ➕ Setup vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
-import { svelte } from "@sveltejs/vite-plugin-svelte"
+/// <reference types="vitest" />
 import { defineConfig } from "vite"
+import { svelte } from "@sveltejs/vite-plugin-svelte"
 import resolve from "@rollup/plugin-node-resolve"
 import commonjs from "@rollup/plugin-commonjs"
-import globals from "rollup-plugin-node-globals"
 
 export default defineConfig({
     plugins: [
         svelte(),
         resolve({ browser: true, dedupe: ["svelte"] }),
         commonjs(),
-        { ...globals(), name: "globals" },
     ],
 })
