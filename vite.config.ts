6b60ee4bd69ff65d7f58c70aab6fd4fec4ecb5d3
Ewen Le Bihan <hey@ewen.works>
2022-04-11 18:56:29 +0200

    ➕ Add ability to load YAML files

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import { defineConfig } from "vite"
 import { svelte } from "@sveltejs/vite-plugin-svelte"
 import resolve from "@rollup/plugin-node-resolve"
 import commonjs from "@rollup/plugin-commonjs"
+import yaml from "@rollup/plugin-yaml"
 
 export default defineConfig({
     plugins: [
         svelte(),
         resolve({ browser: true, dedupe: ["svelte"] }),
         commonjs(),
+        yaml(),
     ],
 })
