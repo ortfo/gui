ca9a697c76093451a9b98c59b51eea7564c45e00
Ewen Le Bihan <hey@ewen.works>
2022-04-15 22:30:47 +0200

    🚧 Add a clear button to search bars

--- frontend/components/SearchBar.svelte
+++ frontend/components/SearchBar.svelte
@@ -7,6 +7,11 @@ export let query: string = ""
 <div class="search-bar">
 	<img src="/assets/icon-search.svg" alt="⌕" class="icon" />
 	<input type="text" bind:value={query} placeholder={$_("Search")} />
+	{#if query !== ""}
+		<button class="clear" data-variant="none" on:click={_ => (query = "")}
+			>×</button
+		>
+	{/if}
 </div>
 
 <style>
@@ -14,10 +19,16 @@ export let query: string = ""
 	display: flex;
 	justify-content: center;
 	align-items: center;
+	position: relative;
 }
 .icon {
 	width: 3em;
 	height: 3em;
 	margin-right: 0.5em;
+}
+button.clear {
+	position: absolute;
+	font-size: 2em;
+	right: 0.25em;
 }
 </style>
