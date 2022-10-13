ff6736524f4672439246520e48da2339428c34bf
Ewen Le Bihan <hey@ewen.works>
2022-10-13 19:55:28 +0200

    🚸 Open external links in the user's default browser

---
+++ frontend/components/ExternalLink.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+import { backend } from "../backend"
+
+export let href: string
+</script>
+
+<a {href} on:click|preventDefault={() => backend.openInBrowser(href)}>
+	<slot>{href}</slot>
+</a>
--- frontend/modals/AboutOrtfo.svelte
+++ frontend/modals/AboutOrtfo.svelte
@@ -4,7 +4,9 @@ import { i18n } from "../actions"
 import { backend } from "../backend"
 import { settings } from "../stores"
 import { createNotificationSpawner } from "../utils"
+import ExternalLink from "../components/ExternalLink.svelte"
 
+const commitLink = `https://github.com/ortfo/gui/commit/${__commitHash__}`
 const notifications = createNotificationSpawner()
 </script>
 
@@ -16,31 +18,49 @@ const notifications = createNotificationSpawner()
 
 <dl>
 	<dt use:i18n>created by</dt>
-	<dd><a href="https://ewen.works">ewen le bihan (ewen-lbh)</a></dd>
+	<dd>
+		<ExternalLink href="https://ewen.works"
+			>ewen le bihan (ewen-lbh)</ExternalLink
+		>
+	</dd>
 	<dt use:i18n>made with</dt>
 	<dd>
-		<a href="https://svelte.dev">Svelte</a>,
-		<a href="https://github.com/webview/webview">webview</a>
+		<ExternalLink href="https://svelte.dev">Svelte</ExternalLink>,
+		<ExternalLink href="https://github.com/webview/webview"
+			>webview</ExternalLink
+		>
 	</dd>
 	<dt use:i18n>code available on github</dt>
-	<dd><a href="https://github.com/ortfo/gui">ortfo/gui</a></dd>
+	<dd>
+		<ExternalLink href="https://github.com/ortfo/gui"
+			>ortfo/gui</ExternalLink
+		>
+	</dd>
 	<dt use:i18n>licensed under</dt>
 	<dd>
-		<a href="https://opensource.org/licenses/GPL-3.0"
-			>GNU Public License, version 3</a
+		<ExternalLink href="https://opensource.org/licenses/GPL-3.0"
+			>GNU Public License, version 3</ExternalLink
 		>
 	</dd>
 </dl>
 
 <p class="commit">
 	<code class="commit-hash"
-		>built from <code
-			on:click={async () => {
-				$settings.poweruser = true
-				await backend.settingsWrite($settings)
-				notifications.add($_(`you are now a power user!`))
-			}}>{__commitHash__}</code
-		>
+		>built from
+		{#if $settings.poweruser}
+			<ExternalLink
+				href={commitLink}
+				><span>{__commitHash__}</span></ExternalLink
+			>
+		{:else}
+			<span
+				on:click={async () => {
+					$settings.poweruser = true
+					await backend.settingsWrite($settings)
+					notifications.add($_(`you are now a power user!`))
+				}}>{__commitHash__}</span
+			>
+		{/if}
 		at {__buildDate__}</code
 	>
 </p>
@@ -70,5 +90,18 @@ dt:not(:first-of-type) {
 }
 .commit {
 	font-size: 0.75em;
+}
+:global(a) {
+	text-underline-position: under;
+	text-underline-offset: -0.125em;
+	text-decoration-thickness: 0.08em;
+	transition: text-decoration-thickness 0.25s ease,
+		text-decoration-offset 0.25s ease;
+}
+:global(a):hover,
+:global(a):focus {
+	text-decoration-color: var(--ortforange);
+	text-decoration-thickness: 0.2em;
+	text-underline-offset: calc(-0.125em - (0.2em - 0.08em) / 2);
 }
 </style>
